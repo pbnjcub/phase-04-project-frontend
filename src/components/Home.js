12b3848872d9652353613ffd72e0343f6cd938a3
Paul Jung <pbnjcub@gmail.com>
2023-03-29 21:26:53 -0400

    added navbar, home and student pages

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Welcome to GradeGuru</h1>
+        <br />
+        <h2>What is GradeGuru?</h2>
+        <p>GradeGuru is a web application that allows teachers to create and manage classes, and students and parents to view their grades.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink} from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div>
+        <ul>
+            <li>
+                <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+            <   NavLink to="/students">Students</NavLink>
+            </li>
+
+        </ul>
+
+    </div>
+  );
+}
+
+export default NavBar;
